92f7836eb7a65178c186ba96157d5deba46462c6
MKING301 <33675411+MKing301@users.noreply.github.com>
2019-01-02 17:14:07 -0500

    Removed name from table Signed-off-by: MKING301

--- database_setup.py
+++ database_setup.py
@@ -18,7 +18,6 @@ class User(Base):
     __tablename__ = 'user'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False)
     picture = Column(String(250))
 
